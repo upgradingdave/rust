f33f850676a004ebc44ae9accf8fd8022e577ec2
Dave Paroulek <upgradingdave@gmail.com>
2025-11-23 15:35:12 -0500

    Refactor modules into separate files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,68 +1,28 @@
-pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+mod front_of_house;
 
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
-    
-    // Order a breakfast in the summer with Rye toast.
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    // Change our mind about what bread we'd like.
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
+mod back_of_house;
 
-    // The next line won't compile if we uncomment it; we're not allowed
-    // to see or modify the seasonal fruit that comes with the meal.
-    //meal.seasonal_fruit = String::from("blueberries");
-    
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
-    
-}
+mod customer {
+    pub fn eat_at_restaurant() {
+        // Absolute path
+        crate::front_of_house::hosting::add_to_waitlist();
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+        // Relative path (doesn't work anymore since `front_of_house` was refactored)
+        //front_of_house::hosting::add_to_waitlist();
 
-        fn seat_at_table() {}
-    }
+        // Order a breakfast in the summer with Rye toast.
+        let mut meal = crate::back_of_house::Breakfast::summer("Rye");
+        // Change our mind about what bread we'd like.
+        meal.toast = String::from("Wheat");
+        println!("I'd like {} toast please", meal.toast);
 
-    mod serving {
-        fn take_order() {}
+        // The next line won't compile if we uncomment it; we're not allowed
+        // to see or modify the seasonal fruit that comes with the meal.
+        //meal.seasonal_fruit = String::from("blueberries");
 
-        fn serve_order() {}
-
-        fn take_payment() {}
+        let order1 = crate::back_of_house::Appetizer::Soup;
+        let order2 = crate::back_of_house::Appetizer::Salad;
     }
 }
 
 fn deliver_order() {}
-
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-    
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-    
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-}
