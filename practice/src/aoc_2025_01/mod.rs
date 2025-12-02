dbea585f76bfc1acc18b9f6524a85c0e9a47a906
Dave Paroulek <upgradingdave@gmail.com>
2025-12-01 21:38:38 -0500

    AoC 2025 Day 1 complete

--- practice/src/aoc_2025_01/mod.rs
+++ practice/src/aoc_2025_01/mod.rs
@@ -2,73 +2,37 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-#[derive(Debug)]
-enum Rotations {
-    LEFT(i32),
-    RIGHT(i32),
-    INVALID
-}
-
-pub fn read_rotations_from_file(path_to_file: &str) -> Result<i32, io::Error> {
+pub fn read_rotations_from_file(path_to_file: &str) -> Result<(i32, i32), io::Error> {
     let file = File::open(path_to_file)?;
     let reader = BufReader::new(file);
     let mut current = 50;
-    let mut answer = 0;
+    let mut answer1 = 0;
+    let mut answer2 = 0;
     
     for line in reader.lines() {
         let line = line?;
-        parse_line(&line);
-
-        let r = parse_line(&line);
-        current = *rotate(&mut current, &r);
+        let dir = line.chars().nth(0).unwrap();
+        let steps = line[1..].parse::<i32>().unwrap();
         
-        //println!("Rotate {:?} to point at {}", &r, current);
+        for _ in 0..steps {
+            match dir {
+                'L' => current = if current - 1 < 0 { 99 } else { current - 1 },
+                'R' => current = (current + 1) % 100,
+                _ => return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid direction")),
+            }
+            
+            if current == 0 {
+                answer2 += 1;
+            }
+        }
+        
+        //println!("Rotate {}{} to point at {}", dir, steps, current);
 
         if current == 0 {
-            answer += 1;
+            answer1 += 1;
         }
 
     }
-    Ok(answer)
-}
-
-fn rotate<'a>(current: &'a mut i32, rotation: &Rotations) -> &'a mut i32 {
-    match rotation {
-        Rotations::LEFT(n) => {
-            *current -= n;
-            match current {
-                x if *x < 0 => {
-                    *x += 100;
-                    x
-                },
-                _ => current,
-            }
-        },
-        Rotations::RIGHT(n) => {
-           *current += n;
-            match current {
-                x if *x > 99 => {
-                    *x -= 100;
-                    x
-                },
-                _ => current,
-            }
-   
-        },
-        Rotations::INVALID => {
-            current
-        },
-    }
-}
-
-fn parse_line(line: &str) -> Rotations {
-    let c = line.chars().nth(0).unwrap();
-    let n = line[1..].parse::<i32>().unwrap();
     
-    match c {
-        'L' => Rotations::LEFT(n),
-        'R' => Rotations::RIGHT(n),
-        _ => Rotations::INVALID,
-    }
+    Ok((answer1, answer2))
 }
-
--- practice/src/main.rs
+++ practice/src/main.rs
@@ -2,8 +2,8 @@ use practice::aoc_2025_01::read_rotations_from_file;
 
 fn main() {
     let answer = read_rotations_from_file("input/2025_01a.txt");
-    println!("Answer: {}", answer.unwrap());
+    println!("Answer: {:?}", answer.unwrap());
     
     let answer = read_rotations_from_file("input/2025_01b.txt");
-    println!("Answer: {}", answer.unwrap());
+    println!("Answer: {:?}", answer.unwrap());
 }
