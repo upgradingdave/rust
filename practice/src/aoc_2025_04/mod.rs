e2fc1572ec2503a07e22178542274a32b8cac8b6
Dave Paroulek <upgradingdave@gmail.com>
2025-12-04 12:28:14 -0500

    AoC 2025 Day 04 Part 1a

--- practice/src/aoc_2025_04/mod.rs
+++ practice/src/aoc_2025_04/mod.rs
@@ -18,7 +18,36 @@ fn parse_grid(filename: &str) -> Result<Vec<Vec<bool>>, std::io::Error> {
 pub fn part1(file: &str) -> Result<u64, std::io::Error> {
     let grid = parse_grid(file)?;
     
-    println!("Grid[0][2] is '@': {}", grid[0][2]);
-    
-    Ok(0)
+    let mut accessible = 0;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            //println!("Grid[{}][{}] is '@': {}", i, j, grid[i][j]);
+            if grid[i][j] {
+              let left_neighbor = i > 0 && grid[i - 1][j];
+              let right_neighbor = i < grid.len() - 1 && grid[i + 1][j];
+              let top_neighbor = j > 0 && grid[i][j - 1];
+              let bottom_neighbor = j < grid[i].len() - 1 && grid[i][j + 1];
+              let left_top_neighbor = i > 0 && j > 0 && grid[i - 1][j - 1];
+              let right_top_neighbor = i > 0 && j < grid[i].len() - 1 && grid[i - 1][j + 1];
+              let left_bottom_neighbor = i < grid.len() - 1 && j > 0 && grid[i + 1][j - 1];
+              let right_bottom_neighbor = i < grid.len() - 1 && j < grid[i].len() - 1 && grid[i + 1][j + 1];
+              
+              let mut count = 0;
+              if left_neighbor { count += 1; }
+              if right_neighbor { count += 1; }
+              if top_neighbor { count += 1; }
+              if bottom_neighbor { count += 1; }
+              if left_top_neighbor { count += 1; }
+              if right_top_neighbor { count += 1; }
+              if left_bottom_neighbor { count += 1; }
+              if right_bottom_neighbor { count += 1; }
+              
+              if count < 4 {
+                  accessible = accessible + 1;
+              }
+            }
+        }
+    }    
+    Ok(accessible)
 }
